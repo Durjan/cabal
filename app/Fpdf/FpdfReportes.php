2fb8840a1800416696945120aaa26fb3d3dc629c
Durjan <durjan.alvarado94@gmail.com>
2022-04-20 23:32:23 -0600

    prueba

--- app/Fpdf/FpdfReportes.php
+++ app/Fpdf/FpdfReportes.php
@@ -13,8 +13,9 @@ class FpdfReportes extends Fpdf{
         if ( $this->PageNo() == 1 ) {
            
             // Logo
-            $this->Image('assets/images/LOGO.png',10,5,60,25); //(x,y,w,h)
+            $this->Image('assets/images/logo.png',10,5,60,25); //(x,y,w,h)
             // Arial bold 15
+            //prueba de linea
             $this->SetFont('Arial','B',22);
             // Movernos a la derecha
             $this->SetXY(80,10);
